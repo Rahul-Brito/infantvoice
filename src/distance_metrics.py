25f200655f1e81d0bc80b954a1b5e849266dc277
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-02-04 18:14:24 -0500

    can run all the dimension reduced data through hausdorf distance calculator to look at distane distribution between particicipant points. I then normalize this to the max hausdorf value which may or may not be a good idea. I can then plot the distributions from the three emb models. If I don't normalize the voxceleb model results in much different distane distributions

---
+++ src/distance_metrics.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+
+from IPython.display import clear_output
+
+from scipy.spatial.distance import directed_hausdorff, euclidean
+
+def hausdorff_distances(emb_2d):
+    # directed or assymetric variant     
+    labels = emb_2d.part_id.unique()
+    len_labels = len(labels)
+    # build empty df
+    pairwise_distances_hausdorff = pd.DataFrame(np.zeros((len_labels, len_labels)) , columns = labels, index=labels)
+    # pairwise_distances_euclidean = pd.DataFrame(np.zeros((len_labels, len_labels)) , columns = labels, index=labels)                          
+
+    # Build df out of X
+    #df = pd.DataFrame(X)
+    #df.columns = ['x1', 'x2']
+    #df['label'] = y
+
+    # Compute pairwise distance between labelled arrays 
+    for row in range(len_labels):
+        for col in range(len_labels):
+            clear_output(wait=True)
+            label_a = labels[row]
+            label_b = labels[col]
+            label_a_values = emb_2d[emb_2d.part_id==label_a][['dim0','dim1']].values
+            label_b_values = emb_2d[emb_2d.part_id==label_b][['dim0','dim1']].values
+            dist_hausdorff = directed_hausdorff(label_a_values,label_b_values)
+            pairwise_distances_hausdorff.iloc[row,col]= dist_hausdorff[0]
+            print("Processing row " + str(row) + ", col " + str(col))
+    #         dist_euclidean = euclidean(label_a_values.mean(axis=0),label_b_values.mean(axis=0))
+    #         pairwise_distances_euclidean.iloc[row,col]= dist_euclidean
+
+    max_haus = pairwise_distances_hausdorff.to_numpy().max()
+    pairwise_distances_hausdorff = pairwise_distances_hausdorff.divide(max_haus)
+    return pairwise_distances_hausdorff
+
+
+
+def euclidean_distances(X,y, labels):
+    # directed or assymetric variant     
+    len_labels = len(labels)
+    # build empty df
+    pairwise_distances_euclidean = pd.DataFrame(np.zeros((len_labels, len_labels)) , columns = labels, index=labels)                          
+
+    # Build df out of X
+    df = pd.DataFrame(X)
+    df.columns = ['x1', 'x2']
+    df['label'] = y
+
+    # Compute pairwise distance between labelled arrays 
+    for row in range(len_labels):
+        for col in range(len_labels):
+            clear_output(wait=True)
+            label_a = labels[row]
+            label_b = labels[col]
+            label_a_values = df[df.label==label_a][['x1','x2']].values
+            label_b_values = df[df.label==label_b][['x1','x2']].values
+            dist_euclidean = euclidean(label_a_values.mean(axis=0),label_b_values.mean(axis=0))
+            pairwise_distances_euclidean.iloc[row,col]= dist_euclidean
+
+
+    return pairwise_distances_euclidean
