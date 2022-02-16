6b016355c6cc390a70957e7180984ddadb38a6ea
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-02-16 16:06:50 -0500

    added the embedding quality method to a module. Scaled it a bit manually to calculate for all embedding/dim_reduc pairs for all techniques in Fig1 of the paper

--- src/.ipynb_checkpoints/distance_metrics-checkpoint.py
+++ src/.ipynb_checkpoints/distance_metrics-checkpoint.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 from IPython.display import clear_output
 
-from scipy.spatial.distance import directed_hausdorff, euclidean, cosine
+import scipy
+
+from sklearn.neighbors import NearestNeighbors
+from scipy.spatial.distance import directed_hausdorff, euclidean, cosine, pdist
 
 def hausdorff_distances(emb_2d):
     # directed or assymetric variant     
@@ -90,4 +93,44 @@ def cos_distance(emb_a):
     pairwise_distances_cosine = pairwise_distances_cosine.divide(max_cos)
     
     return pairwise_distances_cosine
+
+
+def embedding_quality(X, Z, classes, dim_reduc, knn=10, knn_classes=10, subsetsize=1000):
+    #taken directly from https://github.com/berenslab/rna-seq-tsne/blob/master/toy-example.ipynb
+    
+    nbrs1 = NearestNeighbors(n_neighbors=knn).fit(X)
+    ind1 = nbrs1.kneighbors(return_distance=False)
+
+    nbrs2 = NearestNeighbors(n_neighbors=knn).fit(Z)
+    ind2 = nbrs2.kneighbors(return_distance=False)
+
+    intersections = 0.0
+    for i in range(X.shape[0]):
+        intersections += len(set(ind1[i]) & set(ind2[i]))
+    mnn = intersections / X.shape[0] / knn
+    
+    cl, cl_inv = np.unique(classes, return_inverse=True)
+    C = cl.size
+    mu1 = np.zeros((C, X.shape[1]))
+    mu2 = np.zeros((C, Z.shape[1]))
+    for c in range(C):
+        mu1[c,:] = np.mean(X[cl_inv==c,:], axis=0)
+        mu2[c,:] = np.mean(Z[cl_inv==c,:], axis=0)
+        
+    nbrs1 = NearestNeighbors(n_neighbors=knn_classes).fit(mu1)
+    ind1 = nbrs1.kneighbors(return_distance=False)
+    nbrs2 = NearestNeighbors(n_neighbors=knn_classes).fit(mu2)
+    ind2 = nbrs2.kneighbors(return_distance=False)
+    
+    intersections = 0.0
+    for i in range(C):
+        intersections += len(set(ind1[i]) & set(ind2[i]))
+    mnn_global = intersections / C / knn_classes
+    
+    subset = np.random.choice(X.shape[0], size=subsetsize, replace=False)
+    d1 = pdist(X[subset,:])
+    d2 = pdist(Z[subset,:])
+    rho = scipy.stats.spearmanr(d1[:,None],d2[:,None]).correlation
+    
+    return (mnn, mnn_global, rho, dim_reduc)
         
