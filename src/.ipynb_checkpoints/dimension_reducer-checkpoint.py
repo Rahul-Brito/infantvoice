5743756d2e88fd84abb44172c2d2fdbb8a66d4bc
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-02-17 15:16:26 -0500

    split my one script into multiple for my various analysis: extract embeddings from wav files; embedding extraction + dimension reduction +distance quantification for all emb models and dim reduction approaches; embedding extraction+cosine distances at high dim without dim reduc for all models; embedding extraction+leave-one-out+dim reduc+hausdorff dist quantification and comparison. Next step is to create a new script with just one of the models to simplify my analysis and then replicate all important analysis with just one model

--- src/.ipynb_checkpoints/dimension_reducer-checkpoint.py
+++ src/.ipynb_checkpoints/dimension_reducer-checkpoint.py
@@ -6,8 +6,9 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
 import umap
 
-def run_tsne(emb_down, perplexity = 30,init='random',learning_rate=200, metric="euclidean"):
+def run_tsne(emb_down,random_state=42 perplexity = 30,init='random',learning_rate=200, metric="euclidean"):
 
+    #run tsne on the data and return a pd with data label. DEFAULT IS ONE RANDOM SEED instead of different random generations
     tsne = TSNE(n_components=2, perplexity=perplexity, metric=metric, init = init, learning_rate=learning_rate, square_distances=True)
 
     emb_tsne = pd.DataFrame(tsne.fit_transform(emb_down.drop(columns='part_id')), columns = ['dim0', 'dim1'])
