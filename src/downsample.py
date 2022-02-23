558db39289899898aaa5edfe53878e7b0564560e
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-02-22 19:19:14 -0500

    extracted embeddings from the 20 mothers SOhye sent, dated 020422. Tried to run all models of this through the pipeline Emb Dim REduct All Models but got an error when calculating the KNN, etc from the tSNE paper. 'TypeError: '<=' not supported between instances of 'str' and 'int'' on the NeirestNeighbor first call. Will figure out the issue tomorrow

--- src/downsample.py
+++ src/downsample.py
@@ -1,5 +1,8 @@
 import os
 import librosa
+from IPython.display import clear_output
+import soundfile as sf
+
 
 #from pydub import AudioSegment
 #from pyannote.audio.utils.signal import Binarize, Peak
@@ -9,13 +12,13 @@ import librosa
 #AudioSegment.converter='C:\\Users\\MrBrito\\Documents\\ffmpeg\\ffmpeg-2021-04-20-git-718e03e5f2-full_build\\ffmpeg-2021-04-20-git-718e03e5f2-full_build\\bin\\ffmpeg.exe'
 
 
-def downsamp_audio(directory, rate=16000):
+def downsamp_audio(raw, down_samp_dir, rate=16000):
 #Takes a directory of .wav files and downsamples them to rate e.g. rate=16000 means downsampled to 16kHz
-    print("hello")
-    for filename in os.listdir(directory):
+    print("starting")
+    for filename in os.listdir(raw):
         if filename.endswith(".wav"):
             clear_output(wait=True)
             new_rate = rate
             resamp, rate = librosa.load(os.path.join(raw, filename), sr=new_rate)
-            sf.write(os.path.join(directory, filename), resamp, new_rate)
+            sf.write(os.path.join(down_samp_dir, filename), resamp, new_rate)
             print("processing" + str(filename))
