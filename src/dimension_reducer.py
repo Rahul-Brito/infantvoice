8f91af33b0ff64faf939d101cfc25fd4e25d4f48
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-04-01 15:15:18 -0400

    pipeline works on gemaps data. Somehow had to modify umap method (removed all the commented stuff from Dan Low's old method) so taht the labels are added as a numpy array (like my tSNE mthod) otherwise it wasn't working. The clusters from this method look like shit

--- src/dimension_reducer.py
+++ src/dimension_reducer.py
@@ -19,62 +19,12 @@ def run_tsne(emb_down,random_state=42, perplexity = 30,init='random',learning_ra
 
 
 
-def run_umap(X=None, y=None, method = 'unsupervised', scaler=None, neighbor = 10, dist=0.1, metric='correlation', 
-             color_code = None, annotate_names = None, annotate = False, test_set = True, title=None, 
-             savefig_path = False, X_test=None, y_test=None, color_code_test = None, plot=True, plot_centroid_names = True):
+def run_umap(emb_down, neighbor = 10, dist=0.1, metric='correlation'):
     
-    dimreduc = 'UMAP'
-    #reducer = umap.UMAP(n_components=dimension, n_neighbors = neighbor, min_dist=dist,metric=metric,random_state=seed_value) #, TSNE(n_components=k, random_state=seed_value), PCA(n_components=k, random_state=seed_value)]
     reducer = umap.UMAP()
-    reducer_name = 'umap' #, 'tsne', 'pca']
-    X_reduced = reducer.fit_transform(X)
+    X_reduced = reducer.fit_transform(emb_down)
 
-   # #pipeline = Pipeline([
-    #    ('normalization', scaler),
-    #    ('reducer', reducer), ])
-
-    #y_encoded = LabelEncoder().fit_transform(y)
-    #if method == 'supervised':
-    #    X_reduced = reducer.fit_transform(X, y_encoded)
-    #elif method == 'metric_learning':
-        #train
-        #mapper = reducer.fit(X, y_encoded)
-    #    X_reduced = reducer.fit_transform(X, y_encoded)
-    #    #test
-    #    X_reduced_test = reducer.transform(X_test)
-        
-    #elif method == 'unsupervised':
-        #X_reduced = reducer.fit_transform(X)
-
-    # find centroids and labels
     data = pd.DataFrame(X_reduced, columns = ['dim0', 'dim1'])
-    data['part_id'] = X['part_id']
-    #data['label'] = y
-
-    #centers = []
-    #centers_labels = np.unique(y)
-    #for sr in centers_labels:
-    #    data_sr = data[data.label==sr]
-    #    sr_centroid = data_sr[['dim0','dim1']].mean(axis=0)
-    #    centers.append(sr_centroid)
+    data['part_id'] = emb_down['part_id'].to_numpy()
     
-    #centers = np.array(centers)
-    
-    #data.drop('label', axis=1, inplace = True)
-    # Plot in 2D
-    #if plot:
-    #    assert dimension == 2 
-    #    if method == 'metric_learning':
-            # train: first time point
-            #scatter_plot(X_reduced, y, color_code, method, annotate = annotate, title = 'First time step (train set)', savefig_path = savefig_path )
-    #        scatter_plot(X_reduced, y, color_code, method, annotate = annotate, title = 'First time step (train set)')
-            # test: next time points            
-            #scatter_plot(X_reduced_test, y_test, color_code_test, method, annotate = annotate, title = title, savefig_path = savefig_path)
-    #        scatter_plot(X_reduced_test, y_test, color_code_test, method, annotate = annotate, title = title)
-            
-    #    else:
-    #        scatter_plot(X_reduced, y, color_code, method, annotate = annotate, title = title, savefig_path = savefig_path, centers=centers, centers_labels = centers_labels,plot_centroid_names = plot_centroid_names )
-    if method == 'metric_learning':
-        return data, pd.DataFrame(X_reduced_test, columns = ['dim0', 'dim1']), centers, centers_labels
-    else:
-        return data#, centers, centers_labels#, dimreduc
+    return data
