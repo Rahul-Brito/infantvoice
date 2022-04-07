23e652182c641e3ee54211b5c4cf7302d6c479a2
Rahul Brito <78616918+Rahul-Brito@users.noreply.github.com>
2022-04-07 13:56:38 -0400

    moved pacmap into dimension reducer module, removed LDA code, removed tSNE and UMAP hyperparameters from Gasser, consolodated cells for pyannote embeddings into one cell

--- src/dimension_reducer.py
+++ src/dimension_reducer.py
@@ -5,6 +5,7 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
 import umap
+import pacmap
 
 def run_tsne(emb_down,random_state=42, perplexity = 30,init='random',learning_rate=200, metric="euclidean"):
 
@@ -25,6 +26,20 @@ def run_umap(emb_down, neighbor = 10, dist=0.1, metric='correlation'):
     X_reduced = reducer.fit_transform(emb_down)
 
     data = pd.DataFrame(X_reduced, columns = ['dim0', 'dim1'])
+    data['part_id'] = emb_down['part_id'].to_numpy()
+    
+    return data
+
+def run_pacmap(emb_down, n_dims=2, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0):
+    
+    # initializing the pacmap instance
+    # Setting n_neighbors to "None" leads to a default choice shown below in "parameter" section
+    embedder_pacmap = pacmap.PaCMAP(n_dims, n_neighbors, MN_ratio, FP_ratio) 
+
+    # fit the data (The index of transformed data corresponds to the index of the original data)
+    emb_pacmap = embedder_pacmap.fit_transform(emb_down.drop(columns='part_id'), init="pca")
+
+    data = pd.DataFrame(emb_pacmap, columns = ['dim0', 'dim1'])
     data['part_id'] = emb_down['part_id'].to_numpy()
     
     return data
